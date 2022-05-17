7dbf030f16508fb5c5f811fcea26f5e6f1757e27
westorres9 <wester.torre9@gmail.com>
2022-05-17 08:13:44 -0300

    add backend no projeto

---
+++ backend/src/test/java/com/b7web/urna/UrnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b7web.urna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
