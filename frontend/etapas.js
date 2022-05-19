ef96c26e2f1cefe4635e31959d9ae6cfefe6c74f
westorres9 <wester.torres9@gmail.com>
2022-05-19 15:13:25 -0300

    aadd candidatos

--- frontend/etapas.js
+++ frontend/etapas.js
@@ -1,48 +1,137 @@
 let etapas = [
     {
-        titulo: 'VEREADOR',
+        titulo: 'DEPUTADO ESTADUAL',
         numeros: 5,
         candidatos: [
             {
-                numero: '38111',
-                nome: 'Fulano de Tal',
-                partido: 'ABC',
+                numero: '15130',
+                nome: 'Leonidio Bouças',
+                partido: 'PMDB',
                 fotos:[
-                    {url:'38111.jpg', legenda: 'Vereador'}
+                    {url:'leonidioBoucas15130.jpg', legenda: 'Deputado Estadual'}
                 ]
             },
             {
-                numero: '77222',
-                nome: 'Beltrano da Silva',
-                partido: 'DEFG',
+                numero: '11999',
+                nome: 'Luiz Humberto',
+                partido: 'PP',
                 fotos:[
-                    {url:'77222.jpg', legenda: 'Vereador'}
+                    {url:'LuizHumberto11999.jpg', legenda: 'Deputado Estadual'}
                 ]
             },
         ]
     },
     {
-        titulo: 'PREFEITO',
-        numeros: 2,
+        titulo: 'DEPUTADO FEDERAL',
+        numeros: 4,
         candidatos: [
             {
-                numero: '99',
-                nome: 'Ciclano',
-                partido: 'ABC',
-                vice: 'Cic',
+                numero: '1199',
+                nome: 'Odelmo Leao',
+                partido: 'PP',
                 fotos:[
-                    {url:'99.jpg', legenda: 'Prefeito'},
-                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                    {url:'odelmoleao1199.jpg', legenda: 'Deputado Federal'},
+            
                 ]
             },
             {
-                numero: '84',
-                nome: 'Zulano',
-                partido: 'QWERTY',
-                vice: 'Zul',
+                numero: '1510',
+                nome: 'Zaire Rezende',
+                partido: 'PMDB',
                 fotos:[
-                    {url:'84.jpg', legenda: 'Prefeito'},
-                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                    {url:'zaire1510.jpg', legenda: 'Deputado Federal'},
+                ]
+            },
+        ]
+    },{
+        titulo: 'SENADOR',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '133',
+                nome: 'Dilma Roussef',
+                partido: 'PT',
+                fotos:[
+                    {url:'dilma.jpg', legenda: 'Senador'},
+                ]
+            },
+            {
+                numero: '456',
+                nome: 'Aécio Neves',
+                partido: 'PSDB',
+               
+                fotos:[
+                    {url:'aecio.jpg', legenda: 'Senador'},
+                ]
+            },
+        ]
+    },{
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '30',
+                nome: 'Romeu Zema',
+                partido: 'Novo',
+                vice: 'Paulo Brandt',
+                fotos:[
+                    {url:'romeuZema.jpg', legenda: 'Governador'},
+                    {url:'romeuvice.jpg', legenda: 'Vice-Governador', small: true}
+                ]
+            },
+            {
+                numero: '45',
+                nome: 'Antonio Anastasia',
+                partido: 'PSDB',
+                vice: 'Marcos Montes',
+                fotos:[
+                    {url:'AntonioAnastacia45.jpg', legenda: 'Governador'},
+                    {url:'anastasiaVice.jpg', legenda: 'Vice-Governador', small: true}
+                ]
+            },
+        ]
+    },{
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                nome: 'Luis Inacio LULA da Silva',
+                partido: 'PT',
+                vice: 'Geraldo Alkmin',
+                fotos:[
+                    {url:'lula.jpg', legenda: 'Presidente'},
+                    {url:'alkmin.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '17',
+                nome: 'Jair Bolsonaro',
+                partido: 'PSL',
+                vice: 'General Mourao',
+                fotos:[
+                    {url:'bolsonaro.jpg', legenda: 'Presidente'},
+                    {url:'genealMourao.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '51',
+                nome: 'Cabo Daciolo',
+                partido: 'Patriotas',
+                vice: 'Suelene Balduino',
+                fotos:[
+                    {url:'caboDaciolo.jpg', legenda: 'Presidente'},
+                    {url:'dacioloVice.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '50',
+                nome: 'Eneias Carneiro',
+                partido: 'PRONA',
+                vice: 'Humberto Lucena',
+                fotos:[
+                    {url:'eneias.jpg', legenda: 'Presidente'},
+                    {url:'HumbertoLucena.jpg', legenda: 'Vice-Presidente', small: true}
                 ]
             },
         ]
